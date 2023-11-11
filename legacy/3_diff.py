c33b02311f24f12d2618e4baefb8e1d0a63f055c
hugh <aitch25@gmail.com>
2023-11-11 13:30:38 +0900

    code cleaned up

--- legacy/3_diff.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__=="__main__":
-    data = pd.read_csv('./DATA/OSM/logs/path_entire_shinbundang.csv')
-    print(list(data['longitude'].diff()))
-    print(list(data['latitude'].diff()))
-    print(data['longitude'].diff().min())
-    print(data['latitude'].diff().min())
